e77710a02dfdc6cef4f39de5007b96faee976f0f
silentPlus <xuwan.j@gmail.com>
2016-04-11 09:19:39 +0800

    Update OAuth2.java

--- src/main/java/com/linkcos/object/aop/login/OAuth2.java
+++ src/main/java/com/linkcos/object/aop/login/OAuth2.java
@@ -7,16 +7,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-/**
- * 
- * ClassName: NeedAouth2 <br/>
- * Function: 在需要Aouth2的方法前加上这个注解,并将对应的sUser和sFace传入到注解中的参数中
- * date: 2014年12月22日 上午10:06:53 <br/>
- *
- * @author 李国煜
- * @version 
- * @since JDK 1.7
- */
 @Target({ElementType.PARAMETER, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
